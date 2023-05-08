afb87e2718234c5d609f73a6f88d6b09ec10b1c5
Keks <keks@htmlacademy.ru>
2023-05-08 18:55:03 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/menu.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('.navigation__wrapper');
+const menuToggle = document.querySelector('.navigation__toggle');
+const menuButton = document.querySelector('.navigation__button');
+const menuActiveClass = 'navigation__wrapper--active';
+const menuToggleActiveClass = 'navigation__toggle--active';
+
+
+menuButton.addEventListener('click', function () {
+  menuToggle.classList.toggle(menuToggleActiveClass);
+  menu.classList.toggle(menuActiveClass);
+});
